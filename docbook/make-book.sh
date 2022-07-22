ac70bee279084c1fb976e074e0051c0eb7fe0272
Jeffrey Walton <noloader@gmail.com>
2022-07-22 10:12:19 -0400

    Update to include Ubuntu 20 and FOP 2.7

--- docbook/make-book.sh
+++ docbook/make-book.sh
@@ -3,7 +3,7 @@
 # Run the Spell Checker with make-spell.sh
 
 # Name without extensions. The final artifact includes the PDF extension.
-BOOKNAME=docbook
+BOOKNAME=docbook-install
 
 # This works around a bug in FOP. fonts.xml says the fonts base
 # directory is ../, but FOP can only consume ./.
@@ -26,12 +26,12 @@ then
 fi
 
 # Find docbook.xsl if it is not specified
-if [[ -z "$DOCBOOK_XSL" ]]
+if [[ -z "${DOCBOOK_XSL}" ]]
 then
     DOCBOOK_XSL="$(find /usr/share -name 'docbook.xsl' 2>/dev/null | grep -E '/fo/docbook.xsl$' | head -n 1)"
 fi
 
-if [[ ! -e "$DOCBOOK_XSL" ]]
+if [[ ! -e "${DOCBOOK_XSL}" ]]
 then
     echo "docbook.xsl was not found. Exiting."
     echo " You must install stylesheets for the program."
@@ -40,7 +40,7 @@ fi
 
 # This magic allows us to build the DocBook on Ubuntu and Fedora.
 # Ubuntu and Fedora use different paths to docbook.xsl.
-sed "s|!!DOCBOOK_XSL_FILE!!|$DOCBOOK_XSL|g" custom.xsl.in > custom.xsl
+sed "s|!!DOCBOOK_XSL_FILE!!|${DOCBOOK_XSL}|g" custom.xsl.in > custom.xsl
 
 if [[ ! "$(command -v fop)" ]]
 then
@@ -69,44 +69,44 @@ then
     echo "Formatting source code..."
     for file in *.xml
     do
-        if xmllint --format "$file" --output "$file.format"
+        if xmllint --format "${file}" --output "${file}.format"
         then
-            mv "$file.format" "$file"
+            mv "${file}.format" "${file}"
         fi
     done
 fi
 
 echo "Creating formatted object..."
-if ! xsltproc --xinclude custom.xsl book.xml > "$BOOKNAME.fo"
+if ! xsltproc --xinclude custom.xsl book.xml > "${BOOKNAME}.fo"
 then
     echo "Failed to create Formatted Object."
     exit 1
 fi
 
 echo "Creating PDF..."
-if ! fop -fo "$BOOKNAME.fo" -c fonts.xml -dpi 75 -pdf "$BOOKNAME.pdf"
+if ! fop -fo "${BOOKNAME}.fo" -c fonts.xml -dpi 75 -pdf "${BOOKNAME}.pdf"
 then
     echo "Failed to create PDF."
     exit 1
 else
-    rm -f "$BOOKNAME.fo" &>/dev/null
+    rm -f "${BOOKNAME}.fo" &>/dev/null
 fi
 
 echo "Optimizing PDF..."
 # https://stackoverflow.com/q/10450120
-if ! gs -q -o "$BOOKNAME.pdf.opt" -sDEVICE=pdfwrite -dPDFSETTINGS=/screen -dCompatibilityLevel=1.4 "$BOOKNAME.pdf"
+if ! gs -q -o "${BOOKNAME}.pdf.opt" -sDEVICE=pdfwrite -dPDFSETTINGS=/screen -dCompatibilityLevel=1.4 "${BOOKNAME}.pdf"
 then
     echo "Failed to optimize PDF."
     # Not a hard failure. The unoptimized PDF is available.
 else
-    mv "$BOOKNAME.pdf.opt" "$BOOKNAME.pdf"
+    mv "${BOOKNAME}.pdf.opt" "${BOOKNAME}.pdf"
 fi
 
 if [[ -f custom.xsl ]]; then
     rm -f custom.xsl
 fi
 
-echo "Created PDF $BOOKNAME.pdf."
-mv "$BOOKNAME.pdf" ../
+echo "Created PDF ${BOOKNAME}.pdf."
+mv "${BOOKNAME}.pdf" ../
 
 exit 0
